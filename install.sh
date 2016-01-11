7d2be322451fd144cfb7751c35e3aa6034fad7f5
pallamidessi <joseph.pallamidessi@gmail.com>
2016-01-12 00:31:40 +0100

    Project setup

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python-gst-1.0
+sudo apt-get install gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly
+sudo apt-get install python-gobject-2*
+sudo apt-get install portaudio19-dev 
+sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
+sudo apt-get install redis-server redis-tools 
